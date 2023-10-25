a501411ec5103792cf9bb10eab2f7604b13072b4
Vinh <vinhb2007218@student.ctu.edu.vn>
2023-10-25 21:44:03 +0700

    Chinh route update

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -13,7 +13,7 @@ router.route("/favorite")
 
 router.route("/:id")
     .get(contacts.findOne)
-    .post(contacts.update)
+    .put(contacts.update)
     .delete(contacts.delete)
 
 module.exports = router;
